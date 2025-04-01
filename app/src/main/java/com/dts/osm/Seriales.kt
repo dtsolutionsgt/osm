97a6d2e16c1f2b5f9b07401e29502e5328ce648a
dtsolutionsgt <dtsolutionsgt@gmail.com>
2025-04-01 05:54:01 -0600

    ver 1

---
+++ app/src/main/java/com/dts/osm/Seriales.kt
@@ -0,0 +1,66 @@
+package com.dts.osm
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Seriales : PBase() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        try {
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.activity_seriales)
+
+            super.initbase(savedInstanceState)
+
+
+
+        } catch (e:Exception) {
+            msgbox(object : Any() {}.javaClass.enclosingMethod.name+". "+e.message)
+        }
+    }
+
+
+    //region Events
+
+
+    //endregion
+
+    //region Main
+
+
+    //endregion
+
+    //region Dialogs
+
+    fun dialogswitch() {
+        try {
+            when (gl?.dialogid) {
+                0 -> {  }
+            }
+        } catch (e: Exception) {
+            msgbox(object : Any() {}.javaClass.enclosingMethod.name + " . " + e.message)
+        }
+    }
+
+    //endregion
+
+    //region Aux
+
+
+    //endregion
+
+    //region Activity Events
+
+    override fun onResume() {
+        try {
+            super.onResume()
+            gl?.dialogr = Runnable { dialogswitch() }
+
+        } catch (e: Exception) {
+            msgbox(object : Any() {}.javaClass.enclosingMethod.name + " . " + e.message)
+        }
+    }
+
+    //endregion
+
+}
